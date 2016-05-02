7e7c253a7572e64309e91039e5c6fa21fb6174c7
Barbara Darques <Barbara Darques>
2016-05-01 22:03:16 -0300

    MAX_REG modificado para 1000

--- includes/generator.h
+++ includes/generator.h
@@ -16,7 +16,7 @@
 
 #include "registry.h"
 
-#define MAX_REG 100 //numero maximo de registro para importação
+#define MAX_REG 1000 //numero maximo de registro para importação
 #define MARGIN '\n' //delimitador para parser de importação para cada unidade
 #define BORDER '\n' //delimitador para parser de importação para atributos
 #define REG_DELI '#' //delimitador de registro
@@ -25,5 +25,10 @@
 //função irá importar todas as series do arquivo apontado por series
 //e escrever em forma de registro no arquivo de dados db.
 int import(FILE* series, FILE* db);
+int randomPosition ();
+char* readFieldToImport (FILE* pfile, const char border);
+REG* readToImport (FILE* pfile, const int position);
+void* addFieldMem (void* dest, const void* from, unsigned int* size, unsigned int n);
+int importOneRegist (FILE *pfile, const REG* regist);
 
 #endif
